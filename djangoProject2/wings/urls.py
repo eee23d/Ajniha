3fd3edc0e0d3b92aab3dce2789af9e154bb79cfa
eee23d <83975468+eee23d@users.noreply.github.com>
2021-05-16 10:44:01 +0300

    Delete djangoProject2 directory

--- djangoProject2/wings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('signIn',views.signIn,name='signIn'),
-    path('logIn',views.logIn,name='logIn'),
-
-]
